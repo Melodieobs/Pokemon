4e5c72e85d19dc7ce8e3e170025951ce43e66b2e
Melodieobs <melodieobszynski@hotmail.com>
2022-08-23 16:07:32 +0200

    nouvellemiseajour

---
+++ derniere miseajour/jeuxPok/conn.php
@@ -0,0 +1,13 @@
+<?php 
+
+    // Fichier permettant d'établir la connexion à la base de données //
+    // Sont éventuellement à changer la valeur des variables : $hote, $user, $pass et $dbname //
+
+    $hote = "localhost";
+    $user = "root";
+    $pass = "";
+    // $dbname = "projetExercice";
+    $dbname = "projetexercice";
+
+    $connexion = new PDO("mysql:host=$hote; dbname=$dbname", $user, $pass);
+?>
